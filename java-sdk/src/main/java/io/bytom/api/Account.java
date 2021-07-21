9fd34de070cac83e17a4194a370b363a1998dbd3
doraemon <wyjDoraemon@163.com>
2021-07-21 17:39:27 +0800

    fix bug

--- java-sdk/src/main/java/io/bytom/api/Account.java
+++ java-sdk/src/main/java/io/bytom/api/Account.java
@@ -227,8 +227,8 @@ public class Account {
         @SerializedName("change")
         public Boolean change;
 
-        @SerializedName("vaild")
-        public Boolean vaild;
+        @SerializedName("valid")
+        public Boolean valid;
 
         @SerializedName("is_local")
         public Boolean is_local;
