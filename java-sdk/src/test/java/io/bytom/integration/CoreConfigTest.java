bdaa5ff1b2962fe2ee44ad1f649c2e09240c2242
doraemon <wyjDoraemon@163.com>
2021-07-30 16:08:14 +0800

    add chain-status api

--- java-sdk/src/test/java/io/bytom/integration/CoreConfigTest.java
+++ java-sdk/src/test/java/io/bytom/integration/CoreConfigTest.java
@@ -30,6 +30,12 @@ public class CoreConfigTest {
     }
 
     @Test
+    public void testChainStatus() throws Exception {
+        CoreConfig.ChainStatus chainStatus = CoreConfig.getChainStatus(client);
+        Assert.assertNotNull(chainStatus);
+    }
+
+    @Test
     public void testGasRate() throws Exception {
         gasRate = CoreConfig.getGasRate(client);
         Assert.assertNotNull(gasRate);
